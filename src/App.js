e33c6a894573b3523a568a8adb4e88b65d7f5898
shamanthsham459 <163649550+shamanthsham459@users.noreply.github.com>
2024-09-23 00:56:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>Hello, World!</h1><br></br>
+      <h2>This is a React App Deployed using Jenkins with Github into Docker container</h2> 
     </div>
   );
 }
